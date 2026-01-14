199a471799e8157f35819964f470aac6f8e0842a
Quentin691 <quentinfayard05@gmail.com>
2026-01-14 09:12:04 +0100

    ajout du contenue dans les txt (a regrouper encore)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,9 +15,10 @@ export default function Navbar() {
     <ul className="flex gap-16 text-sm">
       <li><Link href="/" className="hover:text-gray-500">Accueil</Link></li>
       <li><Link href="/activites" className="hover:text-gray-500">Activités</Link></li>
-      <li><Link href="/a-propos" className="hover:text-gray-500">À propos</Link></li>
-      <li><Link href="/annonces" className="hover:text-gray-500">Annonces</Link></li>
-      <li><Link href="/contact" className="hover:text-gray-500">Contact</Link></li>
+      <li><Link href="/a-propos-de-nous" className="hover:text-gray-500">À propos</Link></li>
+      <li><Link href="/vente" className="hover:text-gray-500">Vente</Link></li>
+      <li><Link href="/location" className="hover:text-gray-500">Location</Link></li>
+      <li><Link href="/contactez-nous" className="hover:text-gray-500">Contact</Link></li>
     </ul>
 
 
